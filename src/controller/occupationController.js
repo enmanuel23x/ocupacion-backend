bbfdd499c4556a54197cbe33acb6e16518e8f6b2
Enmanuel-Leon <45642674+enmanuel23x@users.noreply.github.com>
2021-05-05 17:55:43 -0400

    fix path

--- src/controller/occupationController.js
+++ src/controller/occupationController.js
@@ -1,4 +1,4 @@
-const config = require('config.json');
+const config = require('../../config.json');
 const jwt = require('jsonwebtoken');
 const models = require('../models');
 const Sequelize = require('sequelize');
